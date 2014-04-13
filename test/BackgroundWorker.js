67e1528e624f9aa58651bc59cbbd8fc32b91c6d9
gorillatron <jornandretangen@gmail.com>
2014-04-13 19:59:21 +0200

    Background Worker Test

---
+++ test/BackgroundWorker.js
@@ -0,0 +1,20 @@
+var BackgroundWorker = require( '../src/BackgroundWorker' )
+
+
+describe( 'BackgroundWorker', function() {
+
+  describe( 'BackgroundWorker#start', function() {
+
+    it( 'should throw exepction if tried to start when allready started', function() {
+      var worker
+
+      worker = new BackgroundWorker()
+
+      worker.start()
+      
+      expect(function(){ worker.start() })
+        .to.throwException()
+    })
+
+  })
+})
