5a36ff438b107a610e2bb00d4d27d735ea1cb76c
gorillatron <jornandretangen@gmail.com>
2014-12-13 10:45:32 +0100

     tests for option.keepAlive and throwing of errorif already executed

--- test/specs/AsyncTask.js
+++ test/specs/AsyncTask.js
@@ -1,4 +1,5 @@
-var AsyncTask         = require( '../../index' )
+var AsyncTask = require( '../../index' )
+var Promise   = require( 'bluebird' )
 
 describe( 'AsyncTask', function() {
 
@@ -13,6 +14,45 @@ describe( 'AsyncTask', function() {
 
       asyncTask.execute(3,3).then(function( result ) {
         expect( result ).to.equal( 6 )
+        done()
+      })
+
+    })
+
+    it('should throw error if executed for the second time', function() {
+      var asyncTask = new AsyncTask({
+        doInBackground: function( a, b ) {
+          return a + b
+        }
+      })
+
+      var exception
+
+      try {
+        asyncTask.execute(3,3)
+        asyncTask.execute(3,3)
+      }
+      catch( e ) {
+        exception = e
+      }
+
+      expect( exception ).to.be.an( Error )
+    })
+
+    it('should be able to execute multipe times if option.keepAlive', function( done ) {
+      var asyncTask = new AsyncTask({
+        keepAlive: true,
+        doInBackground: function( a, b ) {
+          return a + b
+        }
+      })
+
+      Promise.all([
+        asyncTask.execute(1,1),
+        asyncTask.execute(2,2),
+        asyncTask.execute(3,3)
+      ]).then(function(results) {
+        expect(results).to.eql([2,4,6])
         done()
       })
 
