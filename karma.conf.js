e4ba046eb8eca9e58c1f0ae1e73d43866d675c81
gorillatron <jornandretangen@gmail.com>
2014-12-16 15:14:22 +0100

    use firefox in travis

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: [ 'progress', 'osx' ],
+    reporters: [ 'progress' ],
 
 
     // web server port
@@ -66,7 +66,7 @@ module.exports = function(config) {
     // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
     // - PhantomJS
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: [ 'Chrome' ],
+    browsers: [ 'Firefox' ],
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
