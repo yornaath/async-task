284b63a9be255971d5738d13f1cf54df461bf31d
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2016-04-30 21:16:04 +0200

    werk

--- src/AsyncTask.js
+++ src/AsyncTask.js
@@ -5,64 +5,64 @@ import uuid             from 'uuid'
 
 export default class AsyncTask {
 
-	constructor( doInBackground, options ) {
+  constructor( doInBackground, options ) {
 
-		if( typeof doInBackground === 'object' ) {
-	    options = doInBackground
-	    doInBackground = options.doInBackground
-	  }
+    if( typeof doInBackground === 'object' ) {
+      options = doInBackground
+      doInBackground = options.doInBackground
+    }
 
-	  this.options = options || {}
+    this.options = options || {}
 
-	  this.uuid = uuid.v4()
+    this.uuid = uuid.v4()
 
-	  this.hasExecuted = false
-	  this.keepAlive = this.options.keepAlive
-	  this.sharingworker = false
+    this.hasExecuted = false
+    this.keepAlive = this.options.keepAlive
+    this.sharingworker = false
 
-	  this.doInBackground = doInBackground
+    this.doInBackground = doInBackground
 
-	  if( typeof this.doInBackground !== 'function' ) {
-	    console.warn( 'AsyncTask[' + this.uuid  + '].doInBackground is not function', this )
-	  }
+    if( typeof this.doInBackground !== 'function' ) {
+			console.warn( 'AsyncTask[' + this.uuid  + '].doInBackground is not function', this )
+    }
 
-	  if( this.options.worker ) {
-	    this.sharingworker = true
-	    this.setWorker( this.options.worker )
-	  }
-	}
+    if( this.options.worker ) {
+      this.sharingworker = true
+      this.setWorker( this.options.worker )
+    }
+  }
 
-	setWorker(worker) {
-		this.worker = worker
-	  this.worker.define( this.uuid + '::doInBackground', this.doInBackground.toString() )
-	  return this.worker
-	}
+  setWorker(worker) {
+    this.worker = worker
+    this.worker.define( this.uuid + '::doInBackground', this.doInBackground.toString() )
+    return this.worker
+  }
 
-	execute() {
-		var worker, args, taskPromise
+  execute() {
+    var worker, args, taskPromise
 
-	  if( this.hasExecuted && !this.keepAlive ) {
-	    throw new Error( 'Cannot execute a allready executed AsyncTask' )
-	  }
+    if( this.hasExecuted && !this.keepAlive ) {
+      throw new Error( 'Cannot execute a allready executed AsyncTask' )
+    }
 
-	  if( !this.worker ) {
-	    this.setWorker( new BackgroundWorker({}) )
-	  }
+    if( !this.worker ) {
+      this.setWorker( new BackgroundWorker({}) )
+    }
 
-	  this.hasExecuted = true
+    this.hasExecuted = true
 
-	  worker = this.worker
-	  args = Array.prototype.slice.call( arguments )
+    worker = this.worker
+    args = Array.prototype.slice.call( arguments )
 
-	  taskPromise = worker.run( this.uuid + '::doInBackground', args )
+    taskPromise = worker.run( this.uuid + '::doInBackground', args )
 
-	  if( !this.keepAlive && !this.sharingworker ) {
-	    taskPromise
-	      .then(function() { worker.terminate() })
-	      .catch(function() { worker.terminate() })
-	  }
+    if( !this.keepAlive && !this.sharingworker ) {
+      taskPromise
+        .then(function() { worker.terminate() })
+        .catch(function() { worker.terminate() })
+    }
 
-	  return taskPromise
-	}
+    return taskPromise
+  }
 
 }
--- src/test.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-import {AsyncTask} from 'async-task'
-
-var a = new AsyncTask(function() {
-	return true
-})
